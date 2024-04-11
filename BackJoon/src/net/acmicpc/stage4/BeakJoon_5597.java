509771a3059f2a6d3edc5b4fbebadf381626b07c
gihyeokkwon <gihyeokkwon@localhost>
2024-04-11 21:07:34 +0900

    2024/04/11 commit

---
+++ BackJoon/src/net/acmicpc/stage4/BeakJoon_5597.java
@@ -0,0 +1,24 @@
+package net.acmicpc.stage4;
+
+
+import java.util.Scanner;
+
+public class BeakJoon_5597 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] studentarr;
+		studentarr = new int[31];
+		for (int z = 1; z < 29; z++) {
+			int success = sc.nextInt();
+			studentarr[success] = 1;	
+		}
+		
+		for (int j = 1; j < studentarr.length; j++) {
+			if(studentarr[j]!= 1) {
+				System.out.println(j);
+			}
+		}
+	}
+}
+
